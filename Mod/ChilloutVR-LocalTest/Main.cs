074dc0b03ddbecd47cddba04f84a27ffc79b0db9
CMoyuer <1531964875@qq.com>
2022-08-02 00:34:43 +0800

    Support offline test

--- Mod/ChilloutVR-LocalTest/Main.cs
+++ Mod/ChilloutVR-LocalTest/Main.cs
@@ -1,8 +1,13 @@
-﻿using ABI_RC.Core.Player;
+﻿using ABI_RC.Core;
+using ABI_RC.Core.Player;
+using ABI_RC.Core.Savior;
+using ABI_RC.Core.UI;
 using MelonLoader;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.IO;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,14 +24,12 @@ namespace ChilloutVR_LocalTest
 
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
-            // MelonLogger.Msg($"OnSceneWasInitialized: {sceneName}");
             _ = Loom.Current;
             UDPSocketHelper.Interface.OnMessageReceive = OnChangeLocalAvatar;
         }
 
         private void OnChangeLocalAvatar(string ip, string msg)
         {
-            if (PlayerSetup.Instance == null) return;
             Loom.QueueOnMainThread(() =>
             {
                 AssetBundle assetBundle = null;
@@ -36,30 +39,17 @@ namespace ChilloutVR_LocalTest
                     if (info.ContainsKey("type"))
                     {
                         var type = (string)info["type"];
-                        if (type.Equals("change_local_avatar"))
+                        if (type.Equals("skip_login"))
                         {
-                            var path = (string)info["path"];
-                            if (!File.Exists(path)) throw new Exception($"Avatar asset not found: {path}");
-                            MelonLogger.Msg($"Loading local avatar: {path}");
-                            assetBundle = AssetBundle.LoadFromFile(path);
-                            var prefab = assetBundle.LoadAsset<GameObject>(assetBundle.GetAllAssetNames()[0]);
-                            var obj = UnityEngine.Object.Instantiate(prefab);
-                            obj.name = "_CVRAvatar(Clone)";
-                            obj.transform.parent = PlayerSetup.Instance.transform.Find("[PlayerAvatar]") ?? PlayerSetup.Instance.transform;
-                            obj.transform.localPosition = Vector3.zero;
-                            obj.transform.localEulerAngles = Vector3.zero;
-                            PlayerSetup.Instance.ClearAvatar();
-                            PlayerSetup.Instance.SetupAvatar(obj);
-                            MelonLogger.Msg($"Load local avatar Success!");
+                            SkipLogin();
+                        }
+                        else if (type.Equals("change_local_avatar"))
+                        {
+                            LoadLocalAvatar((string)info["path"]);
                         }
                         else if (type.Equals("change_local_world"))
                         {
-                            var path = (string)info["path"];
-                            if (!File.Exists(path)) throw new Exception($"World asset not found: {path}");
-                            MelonLogger.Msg($"Loading local world: {path}");
-                            assetBundle = AssetBundle.LoadFromFile(path);
-                            SceneManager.LoadScene(assetBundle.GetAllScenePaths()[0]);
-                            MelonLogger.Msg($"Load local world Success!");
+                            LoadLocalWorld((string)info["path"]);
                         }
                     }
 
@@ -75,6 +65,134 @@ namespace ChilloutVR_LocalTest
                 }
             });
         }
+        // ------------------------------------------------------------------------------------------------
+        private void LoadLocalAvatar(string path)
+        {
+            if (!PlayerSetup.Instance)
+            {
+                MelonLogger.Warning("You are not logged in yet! You can use the SkipLogin function.");
+                return;
+            }
+            AssetBundle assetBundle = null;
+            try
+            {
+                if (!File.Exists(path)) throw new Exception($"Avatar asset not found: {path}");
+                MelonLogger.Msg($"Loading local avatar: {path}");
+                assetBundle = AssetBundle.LoadFromFile(path);
+                var prefab = assetBundle.LoadAsset<GameObject>(assetBundle.GetAllAssetNames()[0]);
+                var obj = UnityEngine.Object.Instantiate(prefab);
+                obj.name = "_CVRAvatar(Clone)";
+                obj.transform.parent = PlayerSetup.Instance.transform.Find("[PlayerAvatar]") ?? PlayerSetup.Instance.transform;
+                obj.transform.localPosition = Vector3.zero;
+                obj.transform.localEulerAngles = Vector3.zero;
+                PlayerSetup.Instance.ClearAvatar();
+                PlayerSetup.Instance.SetupAvatar(obj);
+                MelonLogger.Msg($"Load local avatar Success!");
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error(e);
+            }
+            finally
+            {
+                if (assetBundle != null)
+                    assetBundle.Unload(false);
+            }
+        }
+        // ------------------------------------------------------------------------------------------------
+        private string tempWorldPath = null;
+        private void LoadLocalWorld(string path)
+        {
+            if (!PlayerSetup.Instance)
+            {
+                MelonLogger.Warning("You are not logged in yet! skipping login...");
+                tempWorldPath = path;
+                SkipLogin();
+                return;
+            }
+            AssetBundle assetBundle = null;
+            try
+            {
+                if (!File.Exists(path)) throw new Exception($"World asset not found: {path}");
+                MelonLogger.Msg($"Loading local world: {path}");
+                assetBundle = AssetBundle.LoadFromFile(path);
+                SceneManager.LoadScene(assetBundle.GetAllScenePaths()[0]);
+                MelonLogger.Msg($"Load local world Success!");
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error(e);
+            }
+            finally
+            {
+                if (assetBundle != null)
+                    assetBundle.Unload(false);
+            }
+        }
+        // ------------------------------------------------------------------------------------------------
+        private void SkipLogin()
+        {
+            if (PlayerSetup.Instance)
+            {
+                MelonLogger.Msg("You are logged in!");
+                return;
+            }
+            var manager = UnityEngine.Object.FindObjectOfType<AuthUIManager>();
+            if (!manager)
+            {
+                MelonLogger.Warning("Currently not in the login interface!");
+                return;
+            }
+            var username = "player";
+            var accessKey = "12345678910";
+            if (!File.Exists(Application.dataPath + "/autologin.profile"))
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(string.Concat(new string[]
+                {
+                                "<LoginProfile>    <Username>",
+                                username,
+                                "</Username>    <AccessKey>",
+                                accessKey,
+                                "</AccessKey></LoginProfile>"
+                }));
+                xmlDocument.Save(Application.dataPath + "/autologin.profile");
+            }
+            MetaPort.Instance.username = username;
+            MetaPort.Instance.accessKey = accessKey;
+            MetaPort.Instance.ownerId = "00000000-0000-0000-0000-000000000000";
+            MetaPort.Instance.homeWorldGuid = "501e2584-ce9a-4570-8c28-ef496e033f5f";
+            MetaPort.Instance.currentAvatarGuid = "17c267db-18c4-4900-bb73-ad323f082640";
+            MetaPort.Instance.VideoResolverHashUrl = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/SHA2-256SUMS";
+            MetaPort.Instance.VideoResolverExecutableUrl = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
 
+            CommonTools.Log(CommonTools.LogLevelType_t.Info, string.Concat(new string[]
+            {
+                    "[CVRGame => ",
+                    base.GetType().Name,
+                    "] ABI Realtime Network Services answered with successful login: Authenticated as ",
+                    username,
+                    "."
+            }), "A0100");
+            manager.loginWithProfileUi.SetActive(true);
+            manager.loginFormUi.SetActive(false);
+            manager.errorBox.SetActive(true);
+            manager.errorHeader.text = "Successfully logged in";
+            manager.errorContent.text = "Successfully authenticated as " + username + ".";
+            MelonCoroutines.Start(LoginOk());
+        }
+
+        private IEnumerator LoginOk()
+        {
+            yield return new WaitForEndOfFrame();
+            MetaPort.Instance.UpdateVideoResolver();
+            yield return SceneManager.LoadSceneAsync("Init");
+            if(tempWorldPath != null)
+            {
+                yield return new WaitForSeconds(2.0f);
+                LoadLocalWorld(tempWorldPath);
+            }
+        }
+        // ------------------------------------------------------------------------------------------------
     }
 }
---
+++ Unity/Assets/Moyuer/Script/CVRLocalTest/SkipLogin.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using static MoyuerLocalTest.LocalTestUtils;
+
+namespace MoyuerLocalTest
+{
+    public class CVRSkipLogin : ScriptableObject
+    {
+        [MenuItem("Moyuer/CVR_LocalTest/Skip Login", false, 1)]
+        private static void StartTest()
+        {
+            SendUDPPacket("{\"type\":\"skip_login\"}");
+        }
+    }
+}
+#endif
